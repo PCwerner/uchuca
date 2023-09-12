df79ab6007869b1d09447b09b7f1ba6aae561589
PCwerner <143636436+PCwerner@users.noreply.github.com>
2023-09-12 14:31:11 -0600

    Delete main directory

--- main/java/uchuca/persistence/crud/TbDepartamentoCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package uchuca.persistence.crud;
-
-import org.springframework.data.repository.CrudRepository;
-import uchuca.persistence.entity.TbDepartamento;
-
-public interface TbDepartamentoCrudRepository extends CrudRepository<TbDepartamento,Integer> {
-}
--- main/java/uchuca/persistence/crud/TbTipoUsuarioCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package uchuca.persistence.crud;
-
-import org.springframework.data.repository.CrudRepository;
-import uchuca.persistence.entity.TbTipoUsuario;
-
-public interface TbTipoUsuarioCrudRepository extends CrudRepository<TbTipoUsuario,Integer> {
-}
